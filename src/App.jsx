19fde7be36380c6118f9b55199e6a699ce7f0c41
abbosbey2002 <abbosbey2002>
2023-09-23 17:50:16 +0500

    Marketplace init

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import { NavLink } from 'react-router-dom';
+import Navbar from './component/Navbar';
+import Header from './component/Header';
+import Service from './component/Service';
+import Footer from './component/Footer';
+
+function App() {
+
+
+  return (
+    <>
+    <Navbar />
+    <Header />
+    <Service />
+    <Footer />
+    </>
+  )
+}
+
+export default App
